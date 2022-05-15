6fe56688e9421f5c625d119910d9818a7ec558a5
Okubasufrank <franklineokubasu51@gmail.com>
2022-05-15 18:05:44 +0300

    this commit is for identing the js file

--- assets/javascript/calc.js
+++ assets/javascript/calc.js
@@ -1,68 +1,68 @@
 //calculation of the total score
 
 $(document).ready(function () {
-    
-    function awardMarks(x){
+
+    function awardMarks(x) {
         $('#results').text(x);
     }
 
-    function awardComments(marks){
-        var tempMarks=marks/50*100;
+    function awardComments(marks) {
+        var tempMarks = marks / 50 * 100;
         var commentHolder;
-        if(tempMarks>=80){
-            commentHolder="Exellently scored"
+        if (tempMarks >= 80) {
+            commentHolder = "Exellently scored"
         }
-        else if(tempMarks>=50 && tempMarks<80){
-            commentHolder="Fairly scored"
-            
+        else if (tempMarks >= 50 && tempMarks < 80) {
+            commentHolder = "Fairly scored"
+
         }
-        else{
-            commentHolder="Poorly  scored"
+        else {
+            commentHolder = "Poorly  scored"
 
         }
 
-$('#comments').text(commentHolder)
+        $('#comments').text(commentHolder)
     }
 
-$(document).on('submit', '#form_', function () {
-    var x=0
+    $(document).on('submit', '#form_', function () {
+        var x = 0
 
-var o1Collection=document.getElementsByName('o1');
-var o2Collection=document.getElementsByName('o2');
-var o3Collection=document.getElementsByName('o3');
-var o4Collection=document.getElementsByName('o4');
-
-
+        var o1Collection = document.getElementsByName('o1');
+        var o2Collection = document.getElementsByName('o2');
+        var o3Collection = document.getElementsByName('o3');
+        var o4Collection = document.getElementsByName('o4');
 
 
-    for (let i of o1Collection) {
-        if (i.checked && i.value == 'correct') {
-          x = x + 10;
+
+
+        for (let i of o1Collection) {
+            if (i.checked && i.value == 'correct') {
+                x = x + 10;
+            }
+
         }
+        for (let i of o2Collection) {
+            if (i.checked && i.value == 'correct') {
+                x = x + 10;
+            }
 
-    }
-    for (let i of o2Collection) {
-        if (i.checked && i.value == 'correct') {
-          x = x + 10;
         }
+        for (let i of o3Collection) {
+            if (i.checked && i.value == 'correct') {
+                x = x + 10;
+            }
 
-    }
-    for (let i of o3Collection) {
-        if (i.checked && i.value == 'correct') {
-          x = x + 10;
         }
+        for (let i of o4Collection) {
+            if (i.checked && i.value == 'correct') {
+                x = x + 10;
+            }
 
-    }
-    for (let i of o4Collection) {
-        if (i.checked && i.value == 'correct') {
-          x = x + 10;
         }
-
-    }
-awardMarks(x);
-awardComments(x);
-   return false;
-});
+        awardMarks(x);
+        awardComments(x);
+        return false;
+    });
 
 })
 
