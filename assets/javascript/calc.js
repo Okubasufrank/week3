440994f091545bf9a03f7fc814e0ae93ec738931
Okubasufrank <franklineokubasu51@gmail.com>
2022-05-15 16:28:43 +0300

    THis commit will add  the answer checking function

--- assets/javascript/calc.js
+++ assets/javascript/calc.js
@@ -3,7 +3,28 @@
 $(document).ready(function () {
 
 $(document).on('submit', '#form_', function () {
-   
+
+var o1Collection=document.getElementsByName('o1');
+var o2Collection=document.getElementsByName('o2');
+var o3Collection=document.getElementsByName('o3');
+var o4Collection=document.getElementsByName('o4');
+
+
+
+
+    var x=0
+    for (let i of o1) {
+        if (i.checked && i.value == 'correct') {
+          x = x + 10;
+        }
+
+    }
+    for (let i of o2) {
+        if (i.checked && i.value == 'correct') {
+          x = x + 10;
+        }
+
+    }
    return false;
 });
 
@@ -11,3 +32,4 @@ $(document).on('submit', '#form_', function () {
         console.log
     }
 })
+
