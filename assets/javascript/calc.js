c6be66126de1acf2763e634e27ad01b5424a992f
Okubasufrank <franklineokubasu51@gmail.com>
2022-05-15 15:27:51 +0300

    this commit is for removing the submission button from div score to div set2

---
+++ assets/javascript/calc.js
@@ -0,0 +1,10 @@
+//calculation of the total score
+
+$(document).on('submit', '#4rm', function () {
+    checkAnswers();
+   alert("working");
+});
+
+    function quizSubmit(){
+        console.log
+    }
